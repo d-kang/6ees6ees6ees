9a3b8b7806f3141a7a0001792a7ada6a25f3a067
d-kang <david.kang714@gmail.com>
2017-07-10 13:43:36 -0700

    Class HoneyMakerBee Extends Bee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    // when you call constructor you must also call super();
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot++;
+  }
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
+
+var honeyMakerBee = new HoneyMakerBee();
+console.log({honeyMakerBee});
