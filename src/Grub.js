f987cd9cc5d9a292799ac18bd7e61129fa034dba
d-kang <david.kang714@gmail.com>
2017-07-10 13:31:00 -0700

    Grub Class Functionality Complete

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,16 @@
 class Grub {
+  constructor() {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+    // this.eat = function(){}
+  }
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
+
+
 };
+
+var grub = new Grub();
+console.log({grub})
