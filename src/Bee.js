6b42e093f4e96b75d30645ac3346da405d41bfd7
d-kang <david.kang714@gmail.com>
2017-07-10 13:40:13 -0700

    Class Bee extends Grub

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,20 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor() {
+    //extending Grub and then calling super allows it to inherit props and methods
+    super();
+    // Must call super constructor in derived class before accessing 'this' or returning from derived constructor
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing'
+  }
 };
+
+
+
+// class mdn docs
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+// super mdn docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
+
+var bee = new Bee();
+console.log({bee});
